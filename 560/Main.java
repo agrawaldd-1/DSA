a73cb62d805eec5b91b096d39dc557e3dabcdf22
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-14 15:53:35 +0530

    solve subarray sum equals k

---
+++ 560/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+    public static  int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0;i <  nums.length;i++){
+            int sum = 0;
+            for(int j = i;j<nums.length;j++){
+                sum += nums[j];
+                if(sum == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3};
+
+        System.out.println(subarraySum(array , 3));
+    }
+}
