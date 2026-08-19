f36c702f8405f4b58098d2b5d35b6baeb6667b14
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-19 14:51:42 +0530

    Solve LeetCode 287 - Find the Duplicate Number

---
+++ 287/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public int findDuplicate(int[] nums) {
+        boolean[] seen = new boolean[nums.length];
+        for(int num:nums){
+            if (seen[num]){
+                return num;
+            
+            }
+            seen[num] = true;
+        }
+        return -1;
+    }
+}
