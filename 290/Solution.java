a9a95fe8ac646112fce6ad5460ff1f3dfd2d8535
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-22 11:58:47 +0530

    Solve LC 290 Word Pattern

---
+++ 290/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.trim().split("\\s+");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+       HashMap<Character, String> charToWord = new HashMap<>();
+        HashMap<String, Character> wordToChar = new HashMap<>(); 
+        for(int i = 0 ; i < pattern.length() ; i++){
+            char p = pattern.charAt(i);
+            String w = words[i];
+            if(charToWord.containsKey(p)){
+                if(!charToWord.get(p).equals(w)){
+                    return false;
+                }
+            }
+            else{
+                charToWord.put(p , w);
+            }
+
+
+            if(wordToChar.containsKey(w)){
+                if(wordToChar.get(w) != p){
+                    return false;
+                }
+            }
+            else{
+                wordToChar.put(w , p);
+            }
+        }
+        return true;
+    }
+}
