f99d0edde9864134798cbe7f96fd687b3a5cc091
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-25 12:53:17 +0530

    Solve LC 7 Reverse Integer

---
+++ 7/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        long reversed = 0;
+        
+        while (x != 0) {
+            int remainder = x % 10;
+            reversed = reversed * 10 + remainder;
+            x = x / 10;
+        }
+        
+        // Check for 32-bit signed integer overflow
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+        
+        return (int) reversed;
+    }
+}
