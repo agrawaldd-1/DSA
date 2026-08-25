1a90711e4cc64c8d27df736bb320b494c24166fe
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-25 12:53:17 +0530

    Solve LC 9 Palindrome Number

---
+++ 9/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int original = x; 
+        int reversed = 0;
+        
+        while (x != 0) {
+            int remainder = x % 10;
+            reversed = reversed * 10 + remainder;
+            x = x / 10;
+        }
+        return reversed == original;
+    }
+}
