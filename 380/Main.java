a005cd5a2498140a9444720faea100515ad3474c
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-25 12:52:34 +0530

    Solve LC 380 Insert Delete GetRandom O(1)

---
+++ 380/Main.java
@@ -0,0 +1,43 @@
+class RandomizedSet {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
+    public RandomizedSet() {
+        
+    }
+    
+    public boolean insert(int val) {
+        if(map.containsKey(val)){
+            return false;
+        }
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!map.containsKey(val)){
+            return false;
+        }
+        int indexToRemove = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(indexToRemove, lastElement);
+        map.put(lastElement, indexToRemove);
+        list.remove(list.size()-1);
+        map.remove(val);
+        return true;
+    }
+    
+    public int getRandom() {
+        Random random = new Random();
+        int index = random.nextInt(list.size());
+        return list.get(index);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
