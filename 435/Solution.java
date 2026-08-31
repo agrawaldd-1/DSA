69f2c5195f3e7f17e380bfa491c7a65d91505be8
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-31 11:49:09 +0530

    Solve LC 435 Non-overlapping Intervals

---
+++ 435/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int currentStart = intervals[0][0];
+        int currentEnd = intervals[0][1];
+        int count = 0;
+        for(int i = 1 ; i < intervals.length ; i++){
+            int nextStart = intervals[i][0];
+            int nextEnd = intervals[i][1];
+
+            if(currentEnd > nextStart){
+                count++;
+            }
+            else{
+                currentEnd = nextEnd;
+            }
+        }
+        return count;   
+    }
+}
